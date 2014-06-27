b0711741e29fea9aab0ea740550f4fcff9a43c13
enlore <n.e.lorenson@gmail.com>
2014-06-26 19:29:35 -0500

    Woooo

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ var express = require('express')
   , less    = require('less-middleware')
   , fs      = require('fs')
   , config  = JSON.parse(fs.readFileSync('config.json'))
+  , bodyParser   = require('body-parser')
   , app     = express()
+  , stripe  = require('stripe')('sk_test_75v3BBW80vcLslnEL5nQmSLM')
+  , Mailgun = require('mailgun').Mailgun
+  , mg      = new Mailgun('key-1le00ub2z3uc8onmlmnk2sdph6-484v5', 'v2')
+  , jade    = require('jade')
   ;
 
 app.set('env', process.env.ENV || 'production')
+
+app.set('sender', 'site@katedyerforjudge.com')
+app.set()
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
@@ -28,9 +36,10 @@ if (app.get('env') === 'production') {
     app.set('port', config.port)
 } else {
     app.set('port', process.env.PORT || 3000)
-    lessOptions.debug = true 
+    lessOptions.debug = false
 }
 
+app.use(bodyParser.urlencoded())
 app.use(less(path.join(__dirname, 'less'), lessOptions))
 app.use(express.static(path.join(__dirname, 'static')))
 
@@ -39,6 +48,38 @@ app.get('/', function (req, res) { res.render('index') })
 app.get('/about', function (req, res) { res.render('about') })
 app.get('/get-involved', function (req, res) { res.render('get-involved') })
 app.get('/donate', function (req, res) { res.render('donate') })
+
+app.post('/donate', function (req, res) {
+    var token = req.body.stripeToken
+    console.log(token)
+
+    stripe.charges.create({
+        amount: 1000,
+        currency: 'usd',
+        card: token,
+        description: 'Test Donation',
+    }, function (err, charge) {
+        if (err && err.type === 'StripeCardError') {
+            console.log('Card declined') 
+            throw Error('Card declined')
+        }
+
+        var emailTemplate = path.join(__dirname, 'views', 'donation-email.jade')
+        var recip = ['n.e.lorenson@gmail.com']
+          , domain = 'katedyerforjudge.com'
+          , subj    = 'New Donation'
+          , text = 'Nick Lorenson\nn.e.lorenson@gmail.com\n$20.00\ntext version'
+          , html = jade.renderFile(emailTemplate, {name: 'Nick Lorenson', email: 'n.e.lorenson@gmail.com', amount: '20.00'})
+          ;
+
+        mg.send(app.get('sender'), recip, subj, text, html, domain, function (err) {
+            if (err)
+               throw err 
+        })
+    })
+
+    return res.redirect('/donate')
+})
 
 app.listen(app.get('port'), function () {
     console.log('~~~~~> Listening on %s', app.get('port'))
---
+++ static/js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    $('#timer').countdown({until: new Date(2014, 8 - 1, 8), compact: true})
+
+    // Stripe
+    Stripe.setPublishableKey('pk_test_gaLOWFmLlT7GpIAzmhMvmubG')
+
+    $donateForm = $("#donate-form").on('submit', function (e) {
+        e.preventDefault() 
+
+        $donateForm.find('[type=submit]').prop('disabled', true)
+
+        Stripe.card.createToken($donateForm, function (stat, response) {
+            if (response.error) {
+                $donateForm.find('.errors').text(response.error.message) 
+                $donateForm.find('[type=submit]').prop('disabled', false)
+            } else {
+                var token = response.id
+                console.log(token)
+
+                $donateForm.append($('<input type="hidden" name="stripeToken">').val(token))
+                e.target.submit()
+            }
+        })
+    })
+
+    // Checkbox donation form handler
+    var $otherCheckbox      = $('#other-checkbox')
+      , $amountSelect       = $('#amount-select')
+      , $otherAmount        = $('<label for="other_amount">Other Amount</label><div class="input-group"><span class="input-group-addon">$</span><input id="other-amount" type="text" class="form-control" name="other_amount"><span class="input-group-addon">.00</span></div>')
+      , $otherContainer     = $('#other-amount-container')
+      ;
+
+    $otherCheckbox.on('change', function (e) {
+        if (this.checked) {
+            $amountSelect.prop('disabled', true) 
+            $otherContainer.append($otherAmount)
+        } else {    
+            $amountSelect.prop('disabled', false) 
+            $otherContainer.children().remove()
+        }
+    })
+
+    $otherAmount.on('change', function (e) {
+    })
+})
