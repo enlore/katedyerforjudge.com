0b9ac84e8a8b544e64a6bbb7bd4be1812462d936
enlore <n.e.lorenson@gmail.com>
2014-07-07 14:03:01 -0500

    Formatting typo

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ var express         = require('express')
   , bodyParser      = require('body-parser')
   , session         = require('express-session')
   , cookieParser    = require('cookie-parser')
-  , csurf           = require('csurf')
+  , csurf           = require('csurf') 
   , app             = express()
-  , stripe          = require('stripe')('sk_test_75v3BBW80vcLslnEL5nQmSLM')
+  , stripe          = require('stripe')(config.stripe_live_secret || 'sk_test_4KdReHf3654dRthCEiHecLyX')
   , Mailgun         = require('mailgun').Mailgun
   , mg              = new Mailgun('key-1le00ub2z3uc8onmlmnk2sdph6-484v5', 'v2')
   , compress        = require('compression')
@@ -26,7 +26,8 @@ app.set('trust proxy', 1)
 app.set('env', process.env.ENV || 'production')
 app.set('sender', config.sender || "app")
 app.set('domain', 'katedyerforjudge.com')
-app.set('email-recip', 'n.e.lorenson@gmail.com')
+app.set('volunteer-recip', config.volunteer_recip || 'n.e.lorenson@gmail.com')
+app.set('donation-recip', config.donation_recip || 'n.e.lorenson@gmail.com')
 
 var lessOptions = {
     debug: false,
@@ -79,17 +80,6 @@ app.use(csurf(csurfOptions))
 app.use(less(path.join(__dirname, 'less'), lessOptions))
 app.use(express.static(path.join(__dirname, 'static')))
 
-app.use(function (req, res, next) {
-    console.log(req.ip)
-    next()
-})
-
-// reporting on tls
-app.use(function (req, res, next) {
-    console.log('secure? %s', req.secure)
-    next()
-})
-
 // csrf template setter
 app.use(function (req, res, next) {
     res.locals.csrf_token = req.csrfToken()
@@ -115,7 +105,7 @@ app.post('/get-involved', function (req, res) {
         interest: req.body.interest
     }
 
-    var recip   = app.get('email-recip')
+    var recip   = app.get('volunteer-recip')
       , domain  = app.get('domain')
       , subj    = 'VOLUNTEER'
       , text    = ''
@@ -155,18 +145,28 @@ app.post('/donate', function (req, res) {
 
 // donation confirmation
 app.get('/confirm', function (req, res) { res.render('confirm-donate', {form: req.session.form}) })
-app.post('/confirm', function (req, res) {
+app.post('/confirm', function (req, res, next) {
+    console.log(req.body)
+
     // gen token on this request
     stripe.charges.create({
-        amount: req.session.form.amount,
+        // amount in CENTS
+        amount: Number(req.session.form.amount) * 100,
+        receipt_email: req.body.email,
         currency: 'usd',
-        card: req.body.token,
+        card: req.body.stripeToken,
         description: 'Donation to Kate Dyer Campaign',
-
+        metadata : {
+            name: req.body.human_name, 
+            email: req.body.email
+        }
     }, function (err, charge) {
         if (err && err.type === 'StripeCardError') {
             console.log('Card declined') 
-            throw Error('Card declined')
+            next(new Error('Card declined'))
+        } else if (err) {
+            console.log(err) 
+            next(err)
         }
 
         var emailTemplate = path.join(__dirname, 'views', 'donation-email.jade')
@@ -183,7 +183,7 @@ app.post('/confirm', function (req, res) {
             amount          : req.session.form.amount
         }
 
-        var recip   = app.get('email-recip')
+        var recip   = app.get('donation-recip')
           , domain  = app.get('domain')
           , subj    = 'DONATION'
           , text    = ''
@@ -197,6 +197,27 @@ app.post('/confirm', function (req, res) {
 
         res.redirect('/thank-you')
     })
+})
+
+app.use(function (err, req, res, next) {
+    var textBody = ''
+
+    if (err.message && err.stack) {
+        textBody = err.message + '\n' + err.stack
+    } else if (err && err.stack) {
+        textBody = err + '\n' + err.stack 
+    } else if (err) {
+        textBody = err 
+    }
+
+    mg.send('error@katedyerforjudge.com', 'n.e.lorenson@gmail.com', 'APP ERROR', textBody, '', 'katedyerforjudge.com', function (err) {
+        if (err) {
+            console.log('Errors within errors')
+            console.log(err.message, '\n', err.stack) 
+        }
+    })
+
+    res.render('error', {error: err.message})
 })
 
 app.listen(app.get('port'), function () {
